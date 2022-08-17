32c6cf0b83282e265df953e3a2ac7073cf97e6ea
Kavin Valli <mail@kavin.me>
2022-08-17 08:20:19 +0530

    chore: new data

--- database/seeders/AlumniSeeder.php
+++ database/seeders/AlumniSeeder.php
@@ -19,9 +19,13 @@ class AlumniSeeder extends Seeder
     public function run()
     {
         $sheetURLS = [
-            'https://docs.google.com/spreadsheets/d/e/2PACX-1vQS2gOWJvB8vAX_KDT1yqdCtPrOVyYbuRJqBvaNpopaal7GJa3HRgMisq0gmycIIojSZ5Xhh8gpSgaJ/pub?gid=1365779210&single=true&output=tsv',
-            'https://docs.google.com/spreadsheets/d/e/2PACX-1vTTNWbi0rM3KCTtvQ4gYxuG7B6aeeY1eMdlcyAkcfIauC4-umIFQt9SHwDBH8bzg16vtkgD9T3YSfzo/pub?gid=2087093845&single=true&output=tsv',
-            'https://docs.google.com/spreadsheets/d/e/2PACX-1vTTNWbi0rM3KCTtvQ4gYxuG7B6aeeY1eMdlcyAkcfIauC4-umIFQt9SHwDBH8bzg16vtkgD9T3YSfzo/pub?gid=1374751234&single=true&output=tsv',
+            /* 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQS2gOWJvB8vAX_KDT1yqdCtPrOVyYbuRJqBvaNpopaal7GJa3HRgMisq0gmycIIojSZ5Xhh8gpSgaJ/pub?gid=1365779210&single=true&output=tsv', */
+            /* 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTTNWbi0rM3KCTtvQ4gYxuG7B6aeeY1eMdlcyAkcfIauC4-umIFQt9SHwDBH8bzg16vtkgD9T3YSfzo/pub?gid=2087093845&single=true&output=tsv', */
+            /* 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTTNWbi0rM3KCTtvQ4gYxuG7B6aeeY1eMdlcyAkcfIauC4-umIFQt9SHwDBH8bzg16vtkgD9T3YSfzo/pub?gid=1374751234&single=true&output=tsv', */
+            '',
+            '',
+            '',
+            'https://docs.google.com/spreadsheets/d/e/2PACX-1vRTDGjhZIrksNshu3HXy82GDPswM5iZzi_IdcQ0wzOrHQKJEOBVoHVpjs2hVGBLUOHDhFvM1-Q7FDVy/pub?gid=192630303&single=true&output=tsv'
             /* 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTTNWbi0rM3KCTtvQ4gYxuG7B6aeeY1eMdlcyAkcfIauC4-umIFQt9SHwDBH8bzg16vtkgD9T3YSfzo/pub?gid=1705908244&single=true&output=tsv', */
             /* 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRTDGjhZIrksNshu3HXy82GDPswM5iZzi_IdcQ0wzOrHQKJEOBVoHVpjs2hVGBLUOHDhFvM1-Q7FDVy/pub?gid=1215260649&single=true&output=tsv', */
             /* 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTTNWbi0rM3KCTtvQ4gYxuG7B6aeeY1eMdlcyAkcfIauC4-umIFQt9SHwDBH8bzg16vtkgD9T3YSfzo/pub?gid=1674780203&single=true&output=tsv', */
@@ -32,39 +36,41 @@ class AlumniSeeder extends Seeder
         for ($i = 0; $i < count($sheetURLS); $i++) {
             Log::info("At sheet " . $i);
             $sheetURL = $sheetURLS[$i];
-            $sheetContent = explode("\r\n", file_get_contents($sheetURL));
-            $lines = array_slice($sheetContent, 1);
-            $now = Carbon::now('Asia/Kolkata');
-            foreach ($lines as $line) {
-                $row = str_getcsv($line, "\t");
-                $args = [
-                    'name' => trim($row[3], " "),
-                    'passing_year' => $row[4],
-                    'mobile' => $row[5],
-                    'created_at' => $now,
-                    'updated_at' => $now,
-                ];
-                if ($i != 5) {
-                    $args['gender'] = $row[6];
-                }
-                $alumni = Alumni::firstOrCreate(
-                    ['email' => $row[1]],
-                    $args,
-                );
-                $event = Event::find($i + 1);
-                $number_of_members = 1;
-                if ($i == 0) {
-                    if ($row[13] == "Yes") {
-                        $number_of_members = 2;
+            if ($sheetURL != "") {
+                $sheetContent = explode("\r\n", file_get_contents($sheetURL));
+                $lines = array_slice($sheetContent, 1);
+                $now = Carbon::now('Asia/Kolkata');
+                foreach ($lines as $line) {
+                    $row = str_getcsv($line, "\t");
+                    $args = [
+                        'name' => trim($row[3], " "),
+                        'passing_year' => $row[4],
+                        'mobile' => $row[5],
+                        'created_at' => $now,
+                        'updated_at' => $now,
+                    ];
+                    if ($i != 5) {
+                        $args['gender'] = $row[6];
                     }
-                } else if ($i == 3) {
+                    $alumni = Alumni::firstOrCreate(
+                        ['email' => $row[1]],
+                        $args,
+                    );
+                    $event = Event::find($i + 1);
                     $number_of_members = 1;
-                } else if ($i == 5) {
-                    $number_of_members = $row[14];
-                } else {
-                    $number_of_members = $row[9];
+                    if ($i == 0) {
+                        if ($row[13] == "Yes") {
+                            $number_of_members = 2;
+                        }
+                    } else if ($i == 3) {
+                        $number_of_members = 1;
+                    } else if ($i == 5) {
+                        $number_of_members = $row[14];
+                    } else {
+                        $number_of_members = $row[9];
+                    }
+                    $alumni->events()->attach($event, ['number_of_members' => $number_of_members, 'number_of_members_final' => $number_of_members]);
                 }
-                $alumni->events()->attach($event, ['number_of_members' => $number_of_members, 'number_of_members_final' => $number_of_members]);
             }
         }
     }
